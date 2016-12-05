656bbdaf98cacf4a1a5ce1dab1ee2d9d801f8874
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-12-05 15:32:21 +0100

    fix some issues with the geotom importer

--- lib/edf/importers/geotom/importer.py
+++ lib/edf/importers/geotom/importer.py
@@ -1,34 +1,55 @@
 import pandas as pd
 import numpy as np
 from edf.containers.ERT import ERT
+from io import StringIO
 
 
 def _parse_wenner_file(filename, settings):
-    with open(filename, 'r') as fid:
-        header = [fid.readline() for i in range(0, 16)]
-        header
+    """Parse a Geotom .wen (Wenner configuration) file
 
-        df = pd.read_csv(
-            fid,
-            delim_whitespace=True,
-            header=None,
-            names=(
-                'elec1_wenner',
-                'a',
-                'rho_a',
-                'c4',
-                'c5',
-                'c6',
-                'c6',
-                'c7',
-                'c8',
-                'c9',
-            ),
-        )
+    Parsing problems
+    ----------------
+
+    Due to column overflows it is necessary to make sure that spaces are
+    present around the ; character. Example:
+
+        8.000    14.000 10835948.70;   0.001  -123.1853  -1.0  23.10.2014
+
+    """
+    # read data
+    with open(filename, 'r') as fid2:
+        geotom_data_orig = fid2.read()
+
+    # replace all ';' by ' ; '
+    geotom_data = geotom_data_orig.replace(';', ' ; ')
+
+    fid = StringIO()
+    fid.write(geotom_data)
+    fid.seek(0)
+
+    header = [fid.readline() for i in range(0, 16)]
+    header
+
+    df = pd.read_csv(
+        fid,
+        delim_whitespace=True,
+        header=None,
+        names=(
+            'elec1_wenner',
+            'a',
+            'rho_a',
+            'c4',
+            'c5',
+            'c6',
+            'c6',
+            'c7',
+            'c8',
+            'c9',
+        ),
+    )
 
     # compute geometric factor using the Wenner formula
     df['K'] = 2 * np.pi * df['a']
-
     df['R'] = df['rho_a'] / df['K']
 
     Am = df['elec1_wenner']
@@ -46,59 +67,27 @@ def _parse_wenner_file(filename, settings):
 
     return df
 
-    # abmn = np.vstack((Am, Bm, Mm, Nm)).T / 2.0 + 1
 
-    # crmod = np.vstack((
-    #     abmn[:, 0] * 1e4 + abmn[:, 1],
-    #     abmn[:, 3] * 1e4 + abmn[:, 2],
-    #     df['R'],
-    #     df['R'] * 0,  # phase == 0
-    # )).T
-
-    # indices = ~np.any(np.isnan(crmod), axis=1)
-    # crmod_clean = crmod[indices, :]
-
-    # cr = pd.DataFrame(
-    #     crmod_clean,
-    #     columns=(
-    #         'ab',
-    #         'mn',
-    #         'r',
-    #         'p'
-    #     ),
-    # )
-    # # filter
-    # cr = cr.query('r <= 200')
-
-    # import IPython
-    # IPython.embed()
-
-    # with open('volt.dat', 'wb') as fid:
-    #     fid.write(
-    #         bytes(
-    #             '{0}\n'.format(cr.shape[0]),
-    #             'UTF-8',
-    #         )
-    #     )
-    #     np.savetxt(fid, cr.values, fmt='%i %i %f %f')
-
-    # # compute electrode positions
-    # # import IPython
-    # # IPython.embed()
-
-    # print(df)
-
-
-def parse_geotom_file(filename, settings):
+def add_file(filename, settings, container=None):
     """
     settings = {
+        timestep: [int|datetime], timestep relating to this measurement
     }
+    container: ERT container to add dataset to
     """
+    timestep = settings.get('timestep', 0)
+
     # Wenner
     if filename.endswith('.wen'):
         data = _parse_wenner_file(filename, settings)
+        # add timestep column
+        data['timestep'] = timestep
     else:
         raise Exception('Not a Wenner file')
 
-    container = ERT(data)
+    if container is None:
+        container = ERT(data)
+    else:
+        container.df = pd.concat((container.df, data))
+
     return container
