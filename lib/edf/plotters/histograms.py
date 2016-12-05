4702be9ead9294892500e2ad19eadf4335ac6be7
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-12-05 15:32:25 +0100

    improve histogram plotting

--- lib/edf/plotters/histograms.py
+++ lib/edf/plotters/histograms.py
@@ -1,29 +1,45 @@
 # import matplotlib as mpl
+import pandas as pd
 import pylab as plt
+import matplotlib as mpl
+mpl.rcParams['font.size'] = 8.0
 import numpy as np
 
 
 def plot_histograms(ertobj, keys):
+    """
+
+    """
+    # you can either provide a DataFrame or en ERT object
+    if isinstance(ertobj, pd.DataFrame):
+        df = ertobj
+    else:
+        df = ertobj.df
+
     figures = []
 
     for key in keys:
-        subdata_raw = ertobj.df[key].values,
-        print(subdata_raw)
-        print(np.isnan(subdata_raw))
+        subdata_raw = df[key].values
         subdata = subdata_raw[~np.isnan(subdata_raw)]
 
-        fig, axes = plt.subplots(1, 2, figsize=(10 / 2.54, 8 / 2.54))
+        fig, axes = plt.subplots(1, 2, figsize=(10 / 2.54, 5 / 2.54))
         ax = axes[0]
         ax.hist(
             subdata,
             100,
         )
+        ax.set_xlabel('{0}'.format(key))
+        ax.set_ylabel('count')
+        ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(5))
 
         ax = axes[1]
         ax.hist(
-            subdata,
+            np.log10(subdata),
             100,
         )
+        ax.set_xlabel(r'$log_{10}(' + '{0}'.format(key) + ')$')
+        ax.set_ylabel('count')
+        ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(5))
 
         fig.tight_layout()
 
