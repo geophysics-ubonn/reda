6929fa31a3d54e2dad0a8b9ab86027e314f4991c
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-01-04 09:30:32 +0100

    add a function to analytically compute K factors (Neumann equation)

--- lib/edf/utils/geometric_factors.py
+++ lib/edf/utils/geometric_factors.py
@@ -2,10 +2,11 @@
 select and execute functions to compute geometric factors according to the
 rcParams variable.
 """
+import numpy as np
 import edf
 
 
-def compute_K(dataframe, settings=None):
+def compute_K_numerical(dataframe, settings=None):
     inversion_code = edf.rcParams.get('geom_factor.inversion_code', 'crtomo')
     if inversion_code == 'crtomo':
         import edf.utils.geom_fac_crtomo as geom_fac_crtomo
@@ -15,3 +16,17 @@ def compute_K(dataframe, settings=None):
             'Inversion code {0} not implemented for K computation'.format(
                 inversion_code
             ))
+
+
+def compute_K_analytical(dataframe, spacing):
+    """Given an electrode spacing, compute geometrical factors using the
+    equation for the homogeneous half-space (Neumann-equation)
+    """
+    r_am = np.abs(dataframe['A'] - dataframe['M']) * spacing
+    r_an = np.abs(dataframe['A'] - dataframe['N']) * spacing
+    r_bm = np.abs(dataframe['B'] - dataframe['M']) * spacing
+    r_bn = np.abs(dataframe['B'] - dataframe['N']) * spacing
+
+    K = 2 * np.pi / (1 / r_am - 1 / r_an - 1 / r_bm + 1 / r_bn)
+
+    dataframe['K'] = K
