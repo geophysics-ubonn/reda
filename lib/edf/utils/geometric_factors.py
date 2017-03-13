9d11cb8481ca117a5a942cdfac99e0e6a38b5db1
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-03-13 18:30:14 +0100

    enhance computing of analytical geometric factors

--- lib/edf/utils/geometric_factors.py
+++ lib/edf/utils/geometric_factors.py
@@ -2,6 +2,7 @@
 select and execute functions to compute geometric factors according to the
 rcParams variable.
 """
+import pandas as pd
 import numpy as np
 import edf
 
@@ -21,12 +22,38 @@ def compute_K_numerical(dataframe, settings=None):
 def compute_K_analytical(dataframe, spacing):
     """Given an electrode spacing, compute geometrical factors using the
     equation for the homogeneous half-space (Neumann-equation)
+
+    If a dataframe is given, use the column (A, B, M, N). Otherwise, expect an
+    Nx4 arrray.
+
+    TODO
+    ----
+
+    Parameters
+    ----------
+    dataframe: pandas.DataFrame or numpy.ndarray
+        Configurations, either as DataFrame
+    spacing: float or numpy.ndarray
+        distance between electrodes. If array, then these are the x-coordinates
+        of the electrodes
     """
-    r_am = np.abs(dataframe['A'] - dataframe['M']) * spacing
-    r_an = np.abs(dataframe['A'] - dataframe['N']) * spacing
-    r_bm = np.abs(dataframe['B'] - dataframe['M']) * spacing
-    r_bn = np.abs(dataframe['B'] - dataframe['N']) * spacing
+    if isinstance(dataframe, pd.DataFrame):
+        configs = dataframe[['A', 'B', 'M', 'N']]
+    else:
+        configs = dataframe
+
+    # r_am = np.abs(dataframe['A'] - dataframe['M']) * spacing
+    # r_an = np.abs(dataframe['A'] - dataframe['N']) * spacing
+    # r_bm = np.abs(dataframe['B'] - dataframe['M']) * spacing
+    # r_bn = np.abs(dataframe['B'] - dataframe['N']) * spacing
+    r_am = np.abs(configs[:, 0] - configs[:, 2]) * spacing
+    r_an = np.abs(configs[:, 0] - configs[:, 3]) * spacing
+    r_bm = np.abs(configs[:, 1] - configs[:, 2]) * spacing
+    r_bn = np.abs(configs[:, 1] - configs[:, 3]) * spacing
 
     K = 2 * np.pi / (1 / r_am - 1 / r_an - 1 / r_bm + 1 / r_bn)
 
-    dataframe['K'] = K
+    if isinstance(dataframe, pd.DataFrame):
+        dataframe['K'] = K
+
+    return K
