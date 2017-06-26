2a7ac3f896c9c59a96512b5837d30ded956f6619
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-06-26 17:51:59 +0200

    add documentation; return K factors when using numerical computation

--- lib/edf/utils/geometric_factors.py
+++ lib/edf/utils/geometric_factors.py
@@ -7,16 +7,58 @@ import numpy as np
 import edf
 
 
+def apply_K(df, K):
+    """Apply the geometric factors to the dataset and compute (apparent)
+    resistivities/conductivities
+    """
+    if 'K' not in df.columns:
+        df['K'] = K
+
+    df['rho_a'] = df['R'] * df['K']
+    df['sigma_a'] = 1.0 / df['rho_a']
+
+    if 'Zt' in df.columns:
+        df['rho_a_complex'] = df['Zt'] * df['K']
+    return df
+
+
 def compute_K_numerical(dataframe, settings=None):
+    """Use a finite-element modeling code to infer geometric factors for meshes
+    with topography or irregular electrode spacings.
+
+    Parameters
+    ----------
+    dataframe: pandas.DataFrame
+        the data frame that contains the data
+    settings: dict
+        The settings required to compute the geometric factors. See examples
+        down below for more information in the required content.
+
+    Returns
+    -------
+    K
+        K factors (are also directly written to the dataframe)
+
+    Examples
+    --------
+
+    settings = {
+        'rho': 100,
+        'elem': 'elem.dat',
+        'elec': 'elec.dat',
+    }
+
+    """
     inversion_code = edf.rcParams.get('geom_factor.inversion_code', 'crtomo')
     if inversion_code == 'crtomo':
         import edf.utils.geom_fac_crtomo as geom_fac_crtomo
-        geom_fac_crtomo.compute_K(dataframe, settings)
+        K = geom_fac_crtomo.compute_K(dataframe, settings)
     else:
         raise Exception(
             'Inversion code {0} not implemented for K computation'.format(
                 inversion_code
             ))
+    return K
 
 
 def compute_K_analytical(dataframe, spacing):
