6e52d95d50a5b08796ab8454ffd96242b9ecdff3
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-11-10 10:45:19 +0100

    fix example

---
+++ Examples/01_DCIP/example1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import IPython
+IPython
+import sys
+import IPython.core.ultratb as ultratb
+sys.excepthook = ultratb.VerboseTB(
+    call_pdb=True,
+)
+
+import pandas as pd
+pd.set_option('display.width', 1000)
+
+import edf
+container = edf.ERT()
+container.import_syscal_dat('data_normal.txt')
+container.import_syscal_dat('data_reciprocal.txt', reciprocals=48)
+
+import edf.utils.geometric_factors as edfK
+K = edfK.compute_K_analytical(container.df, spacing=0.25)
+edfK.apply_K(container.df, K)
+
+import edf.plotters as plotters
+plotters.histograms.plot_histograms(container, ['R', 'rho_a', 'Iab', ])
