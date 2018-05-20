89936184e380951139457598348cd0d491872c30
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-05-20 12:08:36 +0200

    start restructuring the eit device stuff

---
+++ lib/reda/importers/eit_fzj.py
@@ -0,0 +1,19 @@
+"""Import data from the EIT-systems built at the Research Center Jülich (FZJ).
+
+This module is an attempt to unify and extend upon the modules eit40 and eit160
+"""
+
+def read_mat_file(filename):
+    """High level import function that tries to determine the specific version
+    of the data format used.
+
+    Parameters
+    ----------
+    filename: string
+        File path to a .mat matlab filename, as produced by the various
+        versions of the emmt_pp.exe postprocessing program.
+    """
+    mat = sio.loadmat(filename, squeeze_me=True)
+
+    # check the version
+    if mat['MP']['Version'].item() != 'FZJ-EZ-2017':
