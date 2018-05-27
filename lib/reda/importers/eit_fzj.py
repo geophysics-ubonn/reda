a9f1266e0500c94fb6871f4882b51e4d91d4ef5e
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-05-27 20:19:52 +0200

    start structuring the FZJ importers

--- lib/reda/importers/eit_fzj.py
+++ lib/reda/importers/eit_fzj.py
@@ -1,11 +1,13 @@
 """Import data from the EIT-systems built at the Research Center Jülich (FZJ).
 
-This module is an attempt to unify and extend upon the modules eit40 and eit160
+As there is an increasing number of slightly different file formats in use,
+this module acts as an selector for the appropriate import functions.
 """
 import scipy.io as sio
+import reda.importers.eit40 as eit
 
 
-def read_mat_file(filename):
+def _get_file_version(filename):
     """High level import function that tries to determine the specific version
     of the data format used.
 
@@ -14,9 +16,44 @@ def read_mat_file(filename):
     filename: string
         File path to a .mat matlab filename, as produced by the various
         versions of the emmt_pp.exe postprocessing program.
+
+    Returns
+    -------
+    version: string
+        a sanitized version of the file format version
+
     """
     mat = sio.loadmat(filename, squeeze_me=True)
 
     # check the version
-    if mat['MP']['Version'].item() != 'FZJ-EZ-2017':
-        pass
+    return mat['MP']['Version'].item()
+
+
+def eit_mnu0_data(filename, configs):
+    """Import data postprocessed as 3P data (NMU0), i.e., measured towards
+    common ground.
+
+    Parameters
+    ----------
+    filename: string (usually: eit_data_mnu0.mat)
+        filename of matlab file
+    configs: Nx4 numpy.ndarray|filename
+        4P measurements configurations (ABMN) to generate out of the data
+
+    Returns
+    -------
+    df_emd: pandas.DataFrame
+        The generated 4P data
+    df_md: pandas.DataFrame|None
+        MD data (sometimes this data is not imported, then we return None here)
+    """
+    version = _get_file_version(filename)
+    if version == 'FZJ-EZ-14.02.2013':
+        df_emd = eit.read_emd_ez_20140214(filename, configs)
+    else:
+        raise Exception(
+            'The file version "{}" is not supported yet.'.format(
+                version)
+        )
+
+    return df_emd, None
