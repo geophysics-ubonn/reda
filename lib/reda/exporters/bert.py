1e28236fdaeffd642cbe767c59431a4034d3db3a
Florian Wagner <mail@fwagner.info>
2018-07-04 11:02:11 +0200

    Make bert exporter ignore "norrec" and "id" columns if present.

--- lib/reda/exporters/bert.py
+++ lib/reda/exporters/bert.py
@@ -8,6 +8,15 @@ def export_bert(data, electrodes, filename):
     f.write("%d\n" % len(electrodes))
     f.write("# ")
 
+    # Make temporary copies for renaming
+    electrodes = electrodes.copy()
+    data = data.copy()
+
+    # Remove unnecessary columns and rename according to bert conventions
+    # https://gitlab.com/resistivity-net/bert#the-unified-data-format
+    data.drop(["norrec", "id"], axis=1, inplace=True, errors="ignore")
+    data.rename(columns={"rho_a": "rhoa", "error": "err"}, inplace=True)
+
     electrodes.columns = electrodes.columns.str.lower()
     for key in electrodes.keys():
         f.write("%s " % key)
@@ -19,12 +28,20 @@ def export_bert(data, electrodes, filename):
     f.write("%d\n" % len(data))
     f.write("# ")
     data.columns = data.columns.str.lower()
+
+    # Make sure that a, b, m, n are the first 4 columns
+    columns = data.columns.tolist()
+    for c in "abmn":
+        columns.remove(c)
+    columns = list("abmn") + columns
+    data = data[columns]
+
     for key in data.keys():
         f.write("%s " % key)
     f.write("\n")
     for row in data.itertuples(index=False):
         for i, val in enumerate(row):
-            if i < 4: # Account for ABMN TODO: make more elegant
+            if i < 4:
                 f.write("%d " % val)
             else:
                 f.write("%E " % val)
