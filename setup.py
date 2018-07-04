e4fc0514d08a1bc918239b01f362c1d682f85486
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-07-04 16:52:52 +0200

    add some classifiers to setup.py and add author emails

--- setup.py
+++ setup.py
@@ -16,9 +16,15 @@ if __name__ == '__main__':
         long_description=open('README.md', 'r').read(),
         long_description_content_type="text/markdown",
         author='Maximilian Weigand and Florian M. Wagner',
+        author_email='mweigand@geo.uni-bonn.de and wagner@geo.uni-bonn.de',
         license='MIT',
         url='https://github.com/geophysics-ubonn/reda',
         packages=find_packages("lib"),
         package_dir={'': 'lib'},
         install_requires=['numpy', 'scipy', 'pandas', 'matplotlib'],
+        classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
     )
