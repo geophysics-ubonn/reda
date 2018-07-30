b45c7df3f38d41afe9ab71957c5226c1db1437dd
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-07-30 19:53:24 +0200

    still trying to fix the doc issues

--- lib/reda/utils/mpl.py
+++ lib/reda/utils/mpl.py
@@ -82,13 +82,12 @@ def mpl_get_cb_bound_next_to_plot(ax):
     >>> import matplotlib.pyplot as plt
     >>> from reda.utils.mpl import mpl_get_cb_bound_next_to_plot
     >>> fig, ax = plt.subplots()
+    >>> fig.subplots_adjust(right=0.8)
     >>> ax.plot([1, 2, 3], [1, 2, 3], '.-')
     >>> cb_pos = mpl_get_cb_bound_next_to_plot(ax)
     >>> ax1 = fig.add_axes(cb_pos, frame_on=True)
     >>> cmap = mpl.cm.jet_r
     >>> norm = mpl.colors.Normalize(vmin=float(23), vmax=float(33))
-    >>> # cmap = pm.cmap
-    >>> # norm = pm.norm
     >>> cb1 = mpl.colorbar.ColorbarBase(
     ...     ax1,
     ...     cmap=cmap,
@@ -97,7 +96,7 @@ def mpl_get_cb_bound_next_to_plot(ax):
     ... )
     >>> cb1.locator = mpl.ticker.FixedLocator([23, 28, 33])
     >>> cb1.update_ticks()
-    >>> cb1.ax.artists.remove(cb1.outline)    # remove framei
+    >>> cb1.ax.artists.remove(cb1.outline)
     """
     position = ax.get_position()
 
