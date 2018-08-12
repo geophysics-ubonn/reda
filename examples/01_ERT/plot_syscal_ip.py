d63cd433b44aec656385416f80ab798ccd11413c
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-08-12 23:54:45 +0200

    small fixes

--- examples/01_ERT/plot_syscal_ip.py
+++ examples/01_ERT/plot_syscal_ip.py
@@ -7,7 +7,7 @@ Importing Syscal data
 """
 import reda
 ip = reda.TDIP()
-ip.import_syscal_bin('data_syscal_ip/maxi1n.bin')
+ip.import_syscal_bin('data_syscal_ip/data_normal.bin')
 print(ip.data)
 ip.plot_decay_curve(0)
 
