8fa59862c5bcf6116b08141698c7442129dd75f8
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-08-21 21:25:32 +0200

    comment out pickle stuff to prevent building the docs on travis

--- examples/01_ERT/plot_syscal_ip.py
+++ examples/01_ERT/plot_syscal_ip.py
@@ -5,23 +5,29 @@ Importing Syscal IP data
 ========================
 
 """
-import warnings
-warnings.simplefilter('error', FutureWarning)
-
-import os
-import pickle
-
 import reda
-pfile = 'ip.pickle'
-if os.path.isfile(pfile):
-    with open(pfile, 'rb') as fid:
-        ip = pickle.load(fid)
-else:
-    ip = reda.TDIP()
-    ip.import_syscal_bin('data_syscal_ip/data_normal.bin')
-    ip.import_syscal_bin('data_syscal_ip/data_reciprocal.bin', reciprocals=48)
-    with open(pfile, 'wb') as fid:
-        pickle.dump(ip, fid)
+
+###############################################################################
+# a hack to prevent large loading times...
+
+# import os
+# import pickle
+# pfile = 'ip.pickle'
+# if os.path.isfile(pfile):
+#     with open(pfile, 'rb') as fid:
+#         ip = pickle.load(fid)
+# else:
+#     ip = reda.TDIP()
+#     ip.import_syscal_bin('data_syscal_ip/data_normal.bin')
+#     ip.import_syscal_bin(
+#       'data_syscal_ip/data_reciprocal.bin', reciprocals=48)
+#     with open(pfile, 'wb') as fid:
+#         pickle.dump(ip, fid)
+
+###############################################################################
+ip = reda.TDIP()
+ip.import_syscal_bin('data_syscal_ip/data_normal.bin')
+ip.import_syscal_bin('data_syscal_ip/data_reciprocal.bin', reciprocals=48)
 
 
 print(ip.data[['a', 'b', 'm', 'n', 'id', 'norrec']])
