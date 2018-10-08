203edab0841b295a8871c62a22b533a4548ebc97
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-10-08 13:13:44 +0200

    add crtomo importer

---
+++ lib/reda/importers/crtomo.py
@@ -0,0 +1,93 @@
+# *-* coding: utf-8 *-*
+"""Import data in the CRMod/CRTomo format
+"""
+import os
+from glob import glob
+
+import numpy as np
+import pandas as pd
+
+
+def load_mod_file(filename):
+    """Load a .mod file (sometimes also called volt.dat or data.crt). This file
+    contains the number of measurements in the first line, and in the following
+    lines measurements. Each line consists of 4 columns:
+
+    * the first column contains the current injection electrodes a and b,
+      stored as one integer using the equation: a * 1e4 + b
+    * the second column contains the voltage measurement electrodes m and n,
+      stored as one integer using the equation: m * 1e4 + n
+    * the third column contains the measured resistance [Ohm]
+    * the fourth column contains the measured phase value [mrad]
+
+    Parameters
+    ----------
+    filename : string
+        Path of filename to import
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A reda-conform DataFrame
+
+    Examples
+    --------
+
+        import reda
+        import reda.importers.crtomo as cexp
+        df = cexp.load_mod_file('volt_01_0.1Hz.crt')
+        ert = reda.ERT(data=df)
+
+
+    """
+    df_raw = pd.read_csv(
+        filename, skiprows=1, delim_whitespace=True,
+        names=['ab', 'mn', 'r', 'rpha']
+    )
+    df_raw['a'] = np.floor(df_raw['ab'] / 1e4).astype(int)
+    df_raw['b'] = (df_raw['ab'] % 1e4).astype(int)
+    df_raw['m'] = np.floor(df_raw['mn'] / 1e4).astype(int)
+    df_raw['n'] = (df_raw['mn'] % 1e4).astype(int)
+
+    df = df_raw.drop(['ab', 'mn'], axis=1)
+    return df
+
+
+def load_seit_data(directory, frequency_file='frequencies.dat',
+                   data_prefix='volt_'):
+    """Load sEIT data from data directory. This function loads data previously
+    exported from reda using reda.exporters.crtomo.write_files_to_directory
+
+    Parameters
+    ----------
+    directory : string
+        input directory
+    frequency_file : string, optional
+        file (located in directory) that contains the frequencies
+    data_prefix: string, optional
+        for each frequency a corresponding data file must be present in the
+        input directory. Frequencies and files are matched by sorting the
+        frequencies AND the filenames, retrieved using glob and the
+        data_prefix
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A DataFrame suitable for the sEIT container
+
+    """
+    frequencies = np.loadtxt(directory + os.sep + frequency_file)
+    data_files = sorted(glob(directory + os.sep + data_prefix + '*'))
+    # check that the number of frequencies matches the number of data files
+    if frequencies.size != len(data_files):
+        raise Exception(
+            'number of frequencies does not match number of data files')
+
+    # load data
+    data_list = []
+    for frequency, filename in zip(frequencies, data_files):
+        subdata = load_mod_file(filename)
+        subdata['frequency'] = frequency
+        data_list.append(subdata)
+    df = pd.concat(data_list)
+    return df
