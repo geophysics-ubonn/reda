ba605450ac03dfef3a54e4179eeb2329d0b3db3a
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-12-02 12:47:02 +0100

    syscal pro importer: enable electrode transforms

--- lib/reda/importers/iris_syscal_pro.py
+++ lib/reda/importers/iris_syscal_pro.py
@@ -11,6 +11,8 @@ from io import StringIO
 import pandas as pd
 import numpy as np
 
+from reda.importers.utils.decorators import enable_result_transforms
+
 
 def _convert_coords_to_abmn_X(data, **kwargs):
     """The syscal only stores positions for the electrodes. Yet, we need to
@@ -152,33 +154,6 @@ def import_txt(filename, **kwargs):
         data[['a', 'b', 'm', 'n']] = rec_max + 1 - data[['a', 'b', 'm', 'n']]
 
     return data, None, None
-
-import functools
-
-def enable_result_transforms(func):
-    """Decorator that tries to use the object provided using a kwarg called
-    'electrode_transformator' to transform the return values of an import
-    function. It is intended to be used to transform electrode numbers and
-    locations, i.e. for use in roll-along-measurement schemes.
-
-    The transformator object must have a function .transform, which takes three
-    parameters: data, electrode, topography and returns three correspondingly
-    transformed objects.
-    """
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        func_transformator = kwargs.pop('electrode_transformator', None)
-        data, electrodes, topography = func(*args, **kwargs)
-        if func_transformator is not None:
-            print('CALLING TRANSFOMRATOR')
-            data_transformed, electrodes_transformed, topography_transformed = \
-                func_transformator.transform(data, electrodes, topography)
-            return data_transformed, electrodes_transformed, \
-                topography_transformed
-        else:
-            return data, electrodes, topography
-    return wrapper
 
 
 @enable_result_transforms
