e17681db535e804dc8c0b3428999040c3e5e8693
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-12-02 12:48:41 +0100

    eit fzj importer: remove duplicate functions

--- lib/reda/containers/sEIT.py
+++ lib/reda/containers/sEIT.py
@@ -276,102 +276,6 @@ class sEIT(importers):
             fig.tight_layout()
             return fig, axes
 
-    def get_spectrum(self, nr_id=None, abmn=None, plot_filename=None):
-        """Return a spectrum and its reciprocal counter part, if present in the
-        dataset. Optimally, refer to the spectrum by its normal-reciprocal id.
-
-        Returns
-        -------
-        spectrum_nor : :py:class:`reda.eis.plots.sip_response`
-            Normal spectrum. None if no normal spectrum is available
-        spectrum_rec : :py:class:`reda.eis.plots.sip_response` or None
-            Reciprocal spectrum. None if no reciprocal spectrum is available
-
-        """
-        assert nr_id is None or abmn is None
-        # determine nr_id for given abmn tuple
-        if abmn is not None:
-            subdata = self.data.query(
-                'a == {} and b == {} and m == {} and n == {}'.format(*abmn)
-            ).sort_values('frequency')
-
-            if subdata.shape[0] == 0:
-                return None, None
-            # determine the norrec-id of this spectrum
-            nr_id = subdata['id'].iloc[0]
-            print('nr_id', nr_id)
-
-        # get spectra
-        subdata_nor = self.data.query(
-            'id == {} and norrec=="nor"'.format(nr_id)
-        ).sort_values('frequency')
-
-        subdata_rec = self.data.query(
-            'id == {} and norrec=="rec"'.format(nr_id)
-        ).sort_values('frequency')
-
-        # create spectrum objects
-        spectrum_nor = None
-        spectrum_rec = None
-
-        if subdata_nor.shape[0] > 0:
-            spectrum_nor = eis_plot.sip_response(
-                frequencies=subdata_nor['frequency'].values,
-                rmag=subdata_nor['r'],
-                rpha=subdata_nor['rpha'],
-            )
-        if subdata_rec.shape[0] > 0:
-            spectrum_rec = eis_plot.sip_response(
-                frequencies=subdata_rec['frequency'].values,
-                rmag=subdata_rec['r'],
-                rpha=subdata_rec['rpha'],
-            )
-        if plot_filename is not None:
-            if spectrum_nor is not None:
-                fig = spectrum_nor.plot(
-                    plot_filename,
-                    reciprocal=spectrum_rec,
-                    return_fig=True,
-                    title='a: {} b: {} m: {}: n: {}'.format(
-                        *subdata_nor[['a', 'b', 'm', 'n']].values[0, :]
-                    )
-                )
-                plt.close(fig)
-        return spectrum_nor, spectrum_rec
-
-    def plot_all_spectra(self, outdir):
-        """This is a convenience function to plot ALL spectra currently
-        stored in the container. It is useful to asses whether data filters
-        do perform correctly.
-
-        Note that the function just iterates over all ids and plots the
-        corresponding spectra, thus it is slow.
-
-        Spectra a named using the format: \%.2i_spectrum_id_\{\}.png.
-
-        Parameters
-        ----------
-        outdir : string
-            Output directory to store spectra in. Created if it does not
-            exist.
-        """
-        os.makedirs(outdir, exist_ok=True)
-
-        g = self.data.groupby('id')
-        for nr, (name, item) in enumerate(g):
-            print(
-                'Plotting spectrum with id {} ({} / {})'.format(
-                    name, nr, len(g.groups.keys()))
-            )
-            plot_filename = ''.join((
-                outdir + os.sep,
-                '{:04}_spectrum_id_{}.png'.format(nr, name)
-            ))
-            self.get_spectrum(
-                nr_id=name,
-                plot_filename=plot_filename
-            )
-
     def filter_incomplete_spectra(self, flimit=1000, percAccept=85):
         """Remove all data points that belong to spectra that did not retain at
         least **percAccept** percent of the number of data points.
@@ -442,6 +346,8 @@ class sEIT(importers):
             subdata = self.data.query(
                 'a == {} and b == {} and m == {} and n == {}'.format(*abmn)
             ).sort_values('frequency')
+            if subdata.shape[0] == 0:
+                return None, None
 
             # determine the norrec-id of this spectrum
             nr_id = subdata['id'].iloc[0]
