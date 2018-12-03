84c3349373db1bbadff9fdfb0cfc1159ce48c818
Florian Wagner <mail@fwagner.info>
2018-12-03 10:28:17 +0000

    Deploy reda to github.com/geophysics-ubonn/reda.git:gh-pages

--- _downloads/4151c56cfa48bf743447563efc05604e/plot_configurations.py
+++ _downloads/4151c56cfa48bf743447563efc05604e/plot_configurations.py
@@ -9,5 +9,16 @@ from reda import ConfigManager
 configs = ConfigManager(nr_of_electrodes=32)
 
 configs.gen_gradient(skip=10, step=8, vskip=5)
+configs.gen_dipole_dipole(skipc=1)
+configs.gen_gradient()
+configs.gen_schlumberger(15, 16)
 
 print(configs.configs)
+
+###############################################################################
+# export mechanisms:
+import reda
+with reda.CreateEnterDirectory('exported_configs'):
+    configs.to_iris_syscal('syscal_configs.txt')
+    configs.write_configs('abmn.dat')
+    configs.write_crmod_config('config.dat')
--- _downloads/5f461c46a24cd11f87faade2d2b7049a/plot_syscal_dc.py
+++ _downloads/5f461c46a24cd11f87faade2d2b7049a/plot_syscal_dc.py
@@ -6,15 +6,36 @@ Importing Syscal ERT data
 
 """
 import reda
-container = reda.ERT()
+ert = reda.ERT()
 
-container.import_syscal_txt('data_syscal_ert/data_normal.txt')
-container.import_syscal_txt(
+###############################################################################
+# data import:
+
+# note that you should prefer importing the binary data (the importer can
+# import more data)
+ert.import_syscal_txt('data_syscal_ert/data_normal.txt')
+
+# the second data set was measured in a reciprocal configuration by switching
+# the 24-electrode cables on the Syscal Pro input connectors. The parameter
+# "reciprocals" changes electrode notations.
+ert.import_syscal_txt(
     'data_syscal_ert/data_reciprocal.txt', reciprocals=48)
 
-import reda.utils.geometric_factors as edfK
-K = edfK.compute_K_analytical(container.data, spacing=0.25)
-edfK.apply_K(container.data, K)
+# compute geometrical factors using the analytical half-space equation for a
+# spacing of 0.25 m
+ert.compute_K_analytical(spacing=0.25)
 
-import reda.plotters as plotters
-plotters.histograms.plot_histograms(container, ['r', 'rho_a', 'Iab', ])
+###############################################################################
+# create some plots in a subdirectory
+with reda.CreateEnterDirectory('plots'):
+    ert.pseudosection(
+        column='r', filename='pseudosection_log10_r.pdf', log10=True)
+    ert.histogram(['r', 'rho_a', 'Iab', ], filename='histograms.pdf')
+
+###############################################################################
+# export to various data files
+with reda.CreateEnterDirectory('data_export'):
+    # TODO: seems broken
+    # ert.export_bert('data.ohm')
+    # ert.export_pygimli('data.pygimli')
+    ert.export_crtomo('volt.dat')
