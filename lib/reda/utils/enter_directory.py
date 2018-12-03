a7ba368b90ffbe192c690f38413d0a6ddf309b0c
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-12-03 10:09:06 +0100

    add two context manager for entering a directory: reda.EnterDirectory and reda.CreateEnterDirectory. Both managers enter a directory a return to the present working directory after finishing. The latter manager also creates the output directory, if it does not already exist.

---
+++ lib/reda/utils/enter_directory.py
@@ -0,0 +1,26 @@
+import os
+
+
+class EnterDirectory(object):
+    """This is a context manager that enters a given directory and returns to
+    the initial current directory after finishing
+    """
+    def __init__(self, directory):
+        self.pwd = os.getcwd()
+        self.directory = os.path.abspath(directory)
+
+    def __enter__(self):
+        os.chdir(self.directory)
+
+    def __exit__(self, dtype, value, traceback):
+        os.chdir(self.pwd)
+
+
+class CreateEnterDirectory(EnterDirectory):
+    """This is a context manager that enters a given directory and returns to
+    the initial current directory after finishing. If the target directory does
+    not exist, create it.
+    """
+    def __enter__(self):
+        os.makedirs(self.directory, exist_ok=True)
+        os.chdir(self.directory)
