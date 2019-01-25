dc6ec75b77f4bd6460e65f6221dbbfbe991c678e
Florian Wagner <mail@fwagner.info>
2019-01-25 10:47:40 +0100

    Fix pure mpl unit strings (spaces in mathmode are neglected)

--- lib/reda/main/units.py
+++ lib/reda/main/units.py
@@ -16,20 +16,20 @@ rel = {
     },
     'rho_a': {
         'full_name': 'rhoa_[Ohm m]',
-        'label_latex': r'$\rho_a~[\Omega m]$',
-        'label_mpl': r'$\rho_a [\Omega m]$',
+        'label_latex': r'$\rho_a$~[$\Omega$ m]',
+        'label_mpl': r'$\rho_a$ [$\Omega$m]',
         'unit': r'$\Ohm m$',
     },
     'Iab': {
         'full_name': 'Iab_[mA]',
         'label_latex': r'$I_{ab}~[mA]$',
-        'label_mpl': r'$I_{ab} [mA]$',
+        'label_mpl': r'$I_{ab}$ [mA]',
         'unit': r'mA',
     },
     'Vmn': {
         'full_name': 'Vmn_[mV]',
         'label_latex': r'$U_{mn}~[mV]$',
-        'label_mpl': r'$U_{mn} [mV]$',
+        'label_mpl': r'$U_{mn}$ [mV]',
         'unit': r'mV',
     },
     'log_rho': {
@@ -41,19 +41,19 @@ rel = {
     'rho': {
         'full_name': 'rho_[Ohm m]',
         'label_latex': r'$|\rho|~[\Omega\mbox{m}]$',
-        'label_mpl': r'$|\rho| [\Omega\mbox{m}]$',
+        'label_mpl': r'$|\rho|$ [$\Omega$m]',
         'unit': r'\Ohm m',
     },
     'phi': {
         'full_name': 'phi_[mrad]',
         'label_latex': r'$\phi~[\mbox{mrad}]$',
-        'label_mpl': r'$\phi [\mbox{mrad}]$',
+        'label_mpl': r'$\phi$ [mrad]',
         'unit': r'mrad',
     },
     'log_real': {
         'full_name': 'log_real_[S/m]',
         'label_latex': r"$\log_{10}(\sigma'~[\mbox{S/m}])$",
-        'label_mpl': r"$\log_{10}(\sigma' [\mbox{S/m}])$",
+        'label_mpl': r"$\log_{10}$($\sigma$' [S/m])",
         'unit': r'S/m',
     },
     'log_imag': {
