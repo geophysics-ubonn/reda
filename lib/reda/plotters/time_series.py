ef3a94c1ac7639c4159006ba05c721b987beb404
Florian Wagner <mail@fwagner.info>
2019-01-25 10:48:01 +0100

    Add documentation to plot_quadpole_evolution.

--- lib/reda/plotters/time_series.py
+++ lib/reda/plotters/time_series.py
@@ -2,12 +2,29 @@ import numpy as np
 import pandas as pd
 
 import reda.utils.mpl
+
 plt, mpl = reda.utils.mpl.setup()
 
 
-def plot_quadpole_evolution(dataobj, quadpole, cols, threshold,
-                            rolling=False, ax=None):
-    """
+def plot_quadpole_evolution(dataobj, quadpole, cols, threshold=5, rolling=False,
+                            ax=None):
+    """Visualize time-lapse evolution of a single quadropole.
+
+    Parameters
+    ----------
+    dataobj : :py:class:`pandas.DataFrame`
+        DataFrame containing the data. Please refer to the documentation for
+        required columns.
+    quadpole : list of integers
+        Electrode numbers of the the quadropole.
+    cols : str
+        The column/parameter to plot over time.
+    threshold : float
+        Allowed percentage deviation from the rolling standard deviation.
+    rolling : bool
+        Calculate rolling median values (the default is False).
+    ax : mpl.axes
+        Optional axes object to plot to.
     """
     if isinstance(dataobj, pd.DataFrame):
         df = dataobj
@@ -15,8 +32,7 @@ def plot_quadpole_evolution(dataobj, quadpole, cols, threshold,
         df = dataobj.data
 
     subquery = df.query(
-        'a == {0} and b == {1} and m == {2} and n == {3}'.format(*quadpole)
-    )
+        'a == {0} and b == {1} and m == {2} and n == {3}'.format(*quadpole))
     # rhoa = subquery['rho_a'].values
     # rhoa[30] = 300
     # subquery['rho_a'] = rhoa
@@ -27,8 +43,11 @@ def plot_quadpole_evolution(dataobj, quadpole, cols, threshold,
         fig, ax = plt.subplots(1, 1, figsize=(15 / 2.54, 6 / 2.54))
 
     ax.plot(
-        subquery['timestep'], subquery[cols], '.',
-        color='blue', label='valid data',
+        subquery['timestep'],
+        subquery[cols],
+        '.',
+        color='blue',
+        label='valid data',
     )
     if rolling:
         # rolling mean
@@ -52,11 +71,9 @@ def plot_quadpole_evolution(dataobj, quadpole, cols, threshold,
 
         # find all values that deviate by more than X percent from the
         # rolling_m
-        bad_values = (
-            np.abs(np.abs(
-                subquery['rho_a'].values - rolling_m['rho_a'].values
-            ) / rolling_m['rho_a'].values) > threshold
-        )
+        bad_values = (np.abs(
+            np.abs(subquery['rho_a'].values - rolling_m['rho_a'].values) /
+            rolling_m['rho_a'].values) > threshold)
 
         bad = subquery.loc[bad_values]
         ax.plot(
@@ -68,13 +85,9 @@ def plot_quadpole_evolution(dataobj, quadpole, cols, threshold,
             label='discarded data',
         )
 
-    ax.legend(
-        loc='upper center',
-        fontsize=6
-    )
+    ax.legend(loc='upper center', fontsize=6)
     # ax.set_xlim(10, 20)
 
-    ax.set_ylabel(r'$\rho~[\Omega m]$')
+    ax.set_ylabel(r'$\rho_a$ [$\Omega$m]')
     ax.set_xlabel('timestep')
-    ax.set_xlim(3, 96)
     return fig, ax
