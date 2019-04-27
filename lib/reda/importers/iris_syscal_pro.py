b803e801cce2664e99db5e1d4ef61bf0683c1bb5
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2019-04-27 19:26:47 +0200

    Revert "[syscal binary importer] fix bug where jump in measurement numbers lead"
    This reverts commit 8bfabb2e9bf73e53da82ad5bc85285f1e5fc44af.

--- lib/reda/importers/iris_syscal_pro.py
+++ lib/reda/importers/iris_syscal_pro.py
@@ -221,7 +221,7 @@ def import_bin(filename, **kwargs):
         if len(jump) > 0:
             print('WARNING: Jump detected at index: {}'.format(jump))
             print('Removing all subsequent data points')
-            data_raw = data_raw.iloc[0:jump[1], :]
+            data_raw = data_raw.iloc[0:jump[0], :]
 
 
     if data_raw.shape[0] == 0:
