785800da749c0ce590c998e5c5bb6ad604c85752
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2019-04-28 12:47:55 +0200

    move logging facilities from the ERT container to reda.main.logger for better reuse in other containers

--- lib/reda/containers/ERT.py
+++ lib/reda/containers/ERT.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 
 import reda
+from reda.main.logger import LoggingClass
 import reda.exporters.bert as reda_bert_export
 import reda.exporters.crtomo as reda_crtomo_export
 import reda.importers.bert as reda_bert_import
@@ -16,70 +17,6 @@ import reda.utils.fix_sign_with_K as redafixK
 import reda.utils.geometric_factors as redaK
 from reda.utils import has_multiple_timesteps
 from reda.utils.norrec import assign_norrec_to_df, average_repetitions
-
-
-class LogDataChanges():
-    """Context manager that observes the DataFrame of a data container for
-    changes in the number of rows.
-
-    Examples
-    --------
-
-    >>> from reda.testing.containers import ERTContainer
-    >>> from reda.containers.ERT import LogDataChanges
-    >>> with LogDataChanges(ERTContainer):
-    ...     # now change the data
-    ...     ERTContainer.data.loc[0, "r"] = 22
-    ...     ERTContainer.data.query("r < 10", inplace=True)
-    >>> # ERTContainer.print_log()
-    2... - root - INFO - Data change from 22 to 21
-
-    """
-
-    def __init__(
-            self,
-            container,
-            filter_action='default',
-            filter_query="",
-    ):
-        self.container = container
-        self.logger = container.logger
-        self.filter_action = filter_action
-        self.data_size_before = None
-        self.data_size_after = None
-        self.filter_query = filter_query
-
-    def __enter__(self):
-        if self.container.data is None:
-            self.data_size_before = 0
-        else:
-            self.data_size_before = self.container.data.shape[0]
-        return None
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        # make sure we do not execute more code if an exception was raised in
-        # the context
-        # See:
-        # https://docs.python.org/3/reference/datamodel.html#object.__exit__
-        if(
-            exc_type is not None or
-            exc_value is not None or
-            traceback is not None):
-            return
-        self.data_size_after = self.container.data.shape[0]
-        self.logger.info(
-            'Data change from {0} to {1}'.format(
-                self.data_size_before,
-                self.data_size_after,
-            ),
-            extra={
-                'filter_action': self.filter_action,
-                'df_size_before': self.data_size_before,
-                'df_size_after': self.data_size_after,
-                'filter_query': self.filter_query,
-            },
-        )
-
 
 def append_doc_of(fun):
     def decorator(f):
@@ -237,69 +174,6 @@ class Exporters(object):
     def export_crtomo(self, filename):
         """Export to CRTomo-compatible file"""
         reda_crtomo_export.save_block_to_crt(filename, self.data)
-
-
-class ListHandler(logging.Handler):  # Inherit from logging.Handler
-    def __init__(self, log_list):
-        # run the regular Handler __init__
-        logging.Handler.__init__(self)
-        # Our custom argument
-        self.log_list = log_list
-
-    def emit(self, record):
-        # record.message is the log message
-        self.log_list.append(record)
-
-
-class LoggingClass(object):
-    """Set up logging facilities for the containers
-
-    """
-
-    def setup_logger(self):
-        """Setup a logger
-        """
-        self.log_list = []
-        handler = ListHandler(self.log_list)
-
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-
-        logger = logging.getLogger()
-        logger.addHandler(handler)
-
-        logger.setLevel(logging.INFO)
-
-        self.handler = handler
-        self.logger = logger
-
-    def print_log(self):
-        for record in self.log_list:
-            print(self.handler.format(record))
-
-    def print_data_journal(self):
-        print('')
-        print('--- Data Journal Start ---')
-        print('{0}'.format(datetime.datetime.now()))
-        for record in self.log_list:
-            if hasattr(record, 'filter_action'):
-                # print(record)
-                if record.filter_action == 'import':
-                    print('Data was imported from file {0} '.format(
-                        record.filter_query) + '({0} data points)'.format(
-                            record.df_size_after - record.df_size_before))
-                if record.filter_action == 'filter':
-                    print(
-                        'A filter was applied with query "{0}".'.format(
-                            record.filter_query
-                        ) +
-                        ' In total {0} records were removed'.format(
-                            -record.df_size_after + record.df_size_before
-                        )
-                    )
-        print('--- Data Journal End ---')
-        print('')
 
 
 class ERT(LoggingClass, Importers, Exporters):
--- lib/reda/main/logger.py
+++ lib/reda/main/logger.py
@@ -9,3 +9,67 @@ def setup_logger():
         # filename='logfile.log',
         level=logging.DEBUG,
     )
+
+
+class ListHandler(logging.Handler):  # Inherit from logging.Handler
+    def __init__(self, log_list):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Our custom argument
+        self.log_list = log_list
+
+    def emit(self, record):
+        # record.message is the log message
+        self.log_list.append(record)
+
+
+class LoggingClass(object):
+    """Set up logging facilities for the containers
+    """
+
+    def setup_logger(self):
+        """Setup a logger
+        """
+        self.log_list = []
+        handler = ListHandler(self.log_list)
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+
+        logger = logging.getLogger()
+        logger.addHandler(handler)
+
+        logger.setLevel(logging.INFO)
+
+        self.handler = handler
+        self.logger = logger
+
+    def print_log(self):
+        for record in self.log_list:
+            print(self.handler.format(record))
+
+    def print_data_journal(self):
+        print('')
+        print('--- Data Journal Start ---')
+        print('{0}'.format(datetime.datetime.now()))
+        for record in self.log_list:
+            if hasattr(record, 'filter_action'):
+                # print(record)
+                if record.filter_action == 'import':
+                    print('Data was imported from file {0} '.format(
+                        record.filter_query) + '({0} data points)'.format(
+                            record.df_size_after - record.df_size_before))
+                if record.filter_action == 'filter':
+                    print(
+                        'A filter was applied with query "{0}".'.format(
+                            record.filter_query
+                        ) +
+                        ' In total {0} records were removed'.format(
+                            -record.df_size_after + record.df_size_before
+                        )
+                    )
+        print('--- Data Journal End ---')
+        print('')
+
+
