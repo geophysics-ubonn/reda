6b8abbee308eaeb49621da5af7baa026d91fb9ef
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2019-06-17 14:08:34 +0200

    [CR container] add .import_crtomo_data() and .export_to_crtomo_td_manager() functions

--- lib/reda/containers/CR.py
+++ lib/reda/containers/CR.py
@@ -1,6 +1,7 @@
 """Complex-resistivity container
 """
 from reda.containers.TDIP import TDIP
+from reda.importers.crtomo import load_mod_file
 
 import reda.utils.mpl
 
@@ -8,5 +9,26 @@ import reda.utils.mpl
 plt, mpl = reda.utils.mpl.setup()
 
 
-class CR(TDIP):
-    pass
+class ImportersCR(object):
+    def import_crtomo_data(self, filename):
+        """Import a CRTomo-style measurement file (usually: volt.dat).
+
+        Parameters
+        ----------
+        filename : str
+            path to data file
+        """
+        data = load_mod_file(filename)
+        self._add_to_container(data)
+
+
+class CR(TDIP, ImportersCR):
+    def export_to_crtomo_td_manager(self, grid):
+        """Return a ready-initialized tdman object from the CRTomo tools.
+
+        WARNING: Not timestep aware!
+        """
+        import crtomo
+        data = self.data[['a', 'b', 'm', 'n', 'r', 'rpha']]
+        tdman = crtomo.tdMan(grid=grid, volt_data=data)
+        return tdman
