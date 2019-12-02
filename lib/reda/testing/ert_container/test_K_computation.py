4dab6abbdbb9bd3b17a47fa0617956ecea1f5f88
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2019-12-02 11:39:54 +0100

    rework some ert container tests

--- lib/reda/testing/ert_container/test_K_computation.py
+++ lib/reda/testing/ert_container/test_K_computation.py
@@ -5,80 +5,59 @@ import pandas as pd
 import reda
 
 
-def test_neg_K_one():
-    """Test computation and subsequent sign correction for one negative K
-    factor """
+def _get_dataframe():
+    """Return a suitable test dataframe"""
     df = pd.DataFrame(
             [
-                # normals
-                (0, 1, 2, 4, 3, 1.1),
-                (0, 1, 2, 5, 4, 1.2),
-                (0, 1, 2, 6, 5, 1.3),
-                (0, 1, 2, 7, 6, 1.4),
-                (0, 2, 3, 5, 4, 1.5),
-                (0, 2, 3, 6, 5, 1.6),
-                (0, 2, 3, 7, 6, 1.7),
-                (0, 3, 4, 6, 5, 1.8),
-                (0, 3, 4, 7, 6, 1.9),
-                (0, 4, 5, 7, 6, 2.0),
-            ],
-            columns=['timestep', 'a', 'b', 'm', 'n', 'r'],
-        )
-
-    # witch m, n of first configuration to yield a negative k factor, r, and
-    # rho_a value
-    df.iloc[[0], [3, 4]] = df.iloc[[0], [4, 3]].values
-    df.loc[df.index[0], 'r'] *= -1
-
-    ert = reda.ERT(data=df)
-    ert.compute_K_analytical(spacing=1, debug=True)
-
-    assert ert.data.loc[ert.data.index[0], 'r'] >= 0
-    assert ert.data.loc[ert.data.index[0], 'rho_a'] >= 0
-    assert ert.data.loc[ert.data.index[0], 'k'] >= 0
-
-
-def test_neg_K_two():
-    """Test computation and subsequent sign correction for two negative K
-    factor """
-    df = pd.DataFrame(
-            [
-                (0, 1, 2, 4, 3, 1.1, 11.1, 1.1 - 2j),
-                (0, 1, 2, 5, 4, 1.2, 12.2, 1 - 2j),
-                (0, 1, 2, 6, 5, 1.3, 13.2, 1 - 2j),
-                (0, 1, 2, 7, 6, 1.4, 14.3, 1 - 2j),
-                (0, 2, 3, 5, 4, 1.5, 14.7, 1 - 2j),
-                (0, 2, 3, 6, 5, 1.6, 15.3, 1 - 2j),
-                (0, 2, 3, 7, 6, 1.7, 16.3, 1 - 2.4j),
-                (0, 3, 4, 6, 5, 1.8, 17.3, 1.7 - 2j),
-                (0, 3, 4, 7, 6, 1.9, 18.3, 1.2 - 2j),
-                (0, 4, 5, 7, 6, 2.0, 19.3, 1.1 - 2j),
+                (0, 1, 2, 4, 3, 1.1, 11.1, 99.8 - 4.99j),
+                (0, 1, 2, 5, 4, 1.2, 12.2, 99.7 - 4.4j),
+                (0, 1, 2, 6, 5, 1.3, 13.2, 99.7 - 4.2j),
+                (0, 1, 2, 7, 6, 1.4, 14.3, 99.7 - 4.1j),
+                (0, 2, 3, 5, 4, 1.5, 14.7, 99.7 - 4.998j),
+                (0, 2, 3, 6, 5, 1.6, 15.3, 99.7 - 4j),
+                (0, 2, 3, 7, 6, 1.7, 16.3, 99.7 - 4.4j),
+                (0, 3, 4, 6, 5, 1.8, 17.3, 99.7 - 4.2j),
+                (0, 3, 4, 7, 6, 1.9, 18.3, 99.2 - 4j),
+                (0, 4, 5, 7, 6, 2.0, 19.3, 99.1 - 3j),
             ],
             columns=[
                 'timestep', 'a', 'b', 'm', 'n', 'r', 'Vmn', 'Zt',
             ],
         )
+    df['rpha'] = np.arctan2(np.imag(df['Zt']), np.real(df['Zt'])) * 1000
+    return df
 
-    # witch m, n of first configuration to yield a negative k factor, r, and
-    # rho_a value
-    df.iloc[[0, 1], [3, 4]] = df.iloc[[0, 1], [4, 3]].values
-    df.loc[df.index[[0, 1]], ['r', 'Vmn', 'Zt']] *= -1
 
-    ert = reda.ERT(data=df)
-    ert.compute_K_analytical(spacing=1, debug=True)
+def test_neg_K():
+    """Test computation and subsequent sign correction for two negative K
+    factor """
+    df = _get_dataframe()
 
-    assert np.all(
-        ert.data.loc[
-            ert.data.index[[0, 1]], ['r', 'rho_a', 'k', 'Vmn', ]
-        ] >= 0
-    )
-    assert np.all(np.real(ert.data.loc[
-        ert.data.index[[0, 1]], ['Zt']
-    ]) >= 0)
-    assert np.all(np.imag(ert.data.loc[
-        ert.data.index[[0, 1]], ['Zt']
-    ]) <= 0)
+    # test for 1-3 rows that need fixing (i.e., negative K factor + neg. r)
+    for rows_to_changes in ([0], [0, 1], [0, 1, 2]):
+        # witch m, n of first configuration to yield a negative k factor, r,
+        # and rho_a value
+        df.iloc[
+            rows_to_changes, [3, 4]
+        ] = df.iloc[rows_to_changes, [4, 3]].values
+        df.loc[df.index[rows_to_changes], ['r', 'Vmn', 'Zt']] *= -1
+
+        ert = reda.ERT(data=df)
+        ert.compute_K_analytical(spacing=1, debug=True)
+
+        assert np.all(
+            ert.data.loc[
+                ert.data.index[rows_to_changes], ['r', 'rho_a', 'k', 'Vmn', ]
+            ] >= 0
+        )
+        # check real and imaginary parts pf Zt
+        assert np.all(np.real(ert.data.loc[
+            ert.data.index[rows_to_changes], ['Zt']
+        ]) >= 0)
+        assert np.all(np.imag(ert.data.loc[
+            ert.data.index[rows_to_changes], ['Zt']
+        ]) <= 0)
 
 
 if __name__ == '__main__':
-    test_neg_K_two()
+    test_neg_K()
