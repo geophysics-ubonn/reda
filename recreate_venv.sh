561bc6a8e49ebb56e612ceea655465f8f1824859
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-02-21 13:02:31 +0100

    also instal development packages while recreating the venv

--- recreate_venv.sh
+++ recreate_venv.sh
@@ -5,6 +5,7 @@ source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 rmvirtualenv reda
 mkvirtualenv --python /usr/bin/python3 reda
 pip install -r requirements.txt
+pip install -r requirements_dev.txt
 pip install -r doc/requirements_doc.txt
 pip install ipython
 python setup.py develop
