7cc16f09118272e02c45474a38bada574fc2f25b
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-03-15 14:10:53 +0100

    [examples] add a few plotting calls to the 4d visualization example

--- examples/03_sEIT/plot_visualize_4d_data.py
+++ examples/03_sEIT/plot_visualize_4d_data.py
@@ -8,13 +8,7 @@ This is work in progress
 """
 ###############################################################################
 # imports
-import os
-
-import numpy as np
-
 import reda
-
-
 ###############################################################################
 # load the data set
 
@@ -25,7 +19,6 @@ for nr in range(0, 4):
         timestep=nr
     )
 seit.compute_K_analytical(spacing=1)
-
 ###############################################################################
 # Plotting pseudosections
 with reda.CreateEnterDirectory('output_visualize_4d'):
@@ -36,27 +29,48 @@ with reda.CreateEnterDirectory('output_visualize_4d'):
     )
 
 ###############################################################################
+# Plot a single spectrum
+nor, rec = seit.get_spectrum(abmn=[1, 2, 4, 3])
+
+with reda.CreateEnterDirectory('output_visualize_4d'):
+    for timestep, spectrum in nor.items():
+        spectrum.plot(filename='spectrum_1-2_4-3_ts_{}.png'.format(timestep))
+
+with reda.CreateEnterDirectory('output_visualize_4d'):
+    nor, rec, fig = seit.get_spectrum(
+        abmn=[1, 2, 4, 3], plot_filename='specplot.png'
+    )
+
+###############################################################################
+from reda.eis.plots import multi_sip_response
+# important: use the obj_dict parameter to use a dict as input
+multi = multi_sip_response(obj_dict=nor)
+with reda.CreateEnterDirectory('output_visualize_4d'):
+    multi.plot_cre('multiplot_cre.png')
+    multi.plot_cim('multiplot_cim.png')
+    multi.plot_rmag('multiplot_rmag.png')
+    multi.plot_rpha('multiplot_rpha.png')
+
+###############################################################################
 # Histograms
-import reda.plotters.histograms as redaH
 # just used to close the figures to save memory
 import pylab as plt
-
 
 with reda.CreateEnterDirectory('output_visualize_4d'):
     # plot frequencies in one plot
     name, figs = seit.plot_histograms('rho_a', 'frequency')
     for ts, fig in sorted(figs.items()):
-        fig.savefig(name + '_lin_{}.jpg'.format(ts), dpi=300)
+        fig.savefig(name + '_lin_{}.jpg'.format(ts), dpi=200)
         plt.close(fig)
 
     # plot in log10 representation
     name, figs = seit.plot_histograms('rho_a', 'frequency', log10=True)
     for ts, fig in sorted(figs.items()):
-        fig.savefig(name + '_log10_{}.jpg'.format(ts), dpi=300)
+        fig.savefig(name + '_log10_{}.jpg'.format(ts), dpi=200)
         plt.close(fig)
 
     name, figs = seit.plot_histograms('rho_a', 'timestep')
     # plot only each third plot
     for ts, fig in sorted(figs.items())[0::3]:
-        fig.savefig(name + '_{}.jpg'.format(ts), dpi=300)
+        fig.savefig(name + '_{}.jpg'.format(ts), dpi=200)
         plt.close(fig)
