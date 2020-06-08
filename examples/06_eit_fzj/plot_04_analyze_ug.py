3fd3f49d536b37f70a9bf8d22d3a6ef6d20b154c
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-06-08 07:53:50 +0200

    work on example

--- examples/06_eit_fzj/plot_04_analyze_ug.py
+++ examples/06_eit_fzj/plot_04_analyze_ug.py
@@ -10,25 +10,35 @@ import reda.importers.eit_fzj as eit_fzj
 adc_data = eit_fzj.get_adc_data('data_eit_fzj_2013_ug/eit_data_mnu0.mat')
 
 frequencies = list(adc_data.swaplevel(0, 2).groupby('frequency').groups.keys())
+# frequency to plot
+frequency = frequencies[1]
 
-fig, axes = plt.subplots(
-    len(frequencies), 1, figsize=(20 / 2.54, 100 / 2.54),
+fig, ax = plt.subplots(
+    1, 1, figsize=(20 / 2.54, 25 / 2.54),
     sharex=True, sharey=True)
 
-for nr, frequency in enumerate(frequencies):
-    ax = axes[nr]
-    im = ax.imshow(
-        adc_data.swaplevel(
-            0, 1, axis=1
-        ).swaplevel(0, 2, axis=0)['Ug3_1'].loc[frequency].values)
-    ax.set_aspect('auto')
-    cb = fig.colorbar(im, ax=ax)
-    cb.set_label(r'Ug [V]')
 
-for ax in axes[:]:
-    ax.set_ylabel('injection number')
+subdata = adc_data.swaplevel(0, 2).loc[frequency]
+# import IPython
+# IPython.embed()
+# exit()
+im = ax.imshow(
+    subdata.swaplevel(0, 1, axis=1)['Ug3_1'].values,
+    interpolation=None,
+)
+ax.set_aspect('auto')
 
-axes[-1].set_xlabel('ADC Channel')
+ax.set_yticks(list(range(subdata.index.shape[0])))
+ax.set_yticklabels(subdata.index.values)
+
+cb = fig.colorbar(im, ax=ax)
+cb.set_label(r'Ug [V]')
+# ax.set_ylim(0, 10)
+
+ax.set_title('Ug3-1 for {} Hz'.format(frequency))
+ax.set_ylabel('injection number')
+
+ax.set_xlabel('ADC Channel')
 
 fig.tight_layout()
 # fig.savefig('plot_ug3-1.pdf', dpi=300)
