3bd5fb6a3bf492038a1b2d10e683206bde456cdd
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-12-29 20:12:13 +0100

    [ert] fix bug where electrode positions were not properly handled for syscal txt import

--- lib/reda/containers/ERT.py
+++ lib/reda/containers/ERT.py
@@ -68,11 +68,11 @@ class ERTImporters(ImportersBase):
             del (kwargs['timestep'])
         self.logger.info('IRIS Syscal Pro text import')
         with LogDataChanges(self, filter_action='import'):
-            data, electrodes, topography = reda_syscal.import_txt(
+            data, electrode_positions, topography = reda_syscal.import_txt(
                 filename, **kwargs)
             if timestep is not None:
                 data['timestep'] = timestep
-            self._add_to_container(data)
+            self._add_to_container(data, electrode_positions, topography)
         if kwargs.get('verbose', False):
             print('Summary:')
             self._describe_data(data)
