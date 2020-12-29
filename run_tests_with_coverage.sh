fa85ee4b34b0386d7308a4e05ec4f0e40643d597
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-12-29 10:14:51 +0100

    [test coverage script] also include tests from the reda-testing repository

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
-pytest --cov=reda --cov-report html -v -rsxX --color yes --doctest-modules --durations 5 lib/reda/
-firefox htmlcov/index.html
+pytest \
+	--cov=reda --cov-report html -v -rsxX --color yes \
+	--doctest-modules \
+	--durations 5 \
+	lib/reda \
+	../reda_testing/devices/
+   	# ../reda_testing
+	# lib/reda/
+# firefox htmlcov/index.html
