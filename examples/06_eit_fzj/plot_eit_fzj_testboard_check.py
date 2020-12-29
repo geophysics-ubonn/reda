893bdb9e61c9112c22093e320065fb5ccba7e6e4
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-12-29 10:16:03 +0100

    tweak examples

--- examples/06_eit_fzj/plot_eit_fzj_testboard_check.py
+++ examples/06_eit_fzj/plot_eit_fzj_testboard_check.py
@@ -1,17 +1,21 @@
-#%%
-'''
+#!/usr/bin/env python
+
+"""
+EIT40 Testboard V2
+------------------
+
 This example shows how to execute the helper function "testboard_evaluation"
 from the eit_fzj_utils. It is used to plot the results of measurements
 conducted on the testboard that was built to check the functionality of the
 eit systems. A theoretical calculated response that is based on the built-in
 resistors and capicitors is plotted together with the measurement results.
-'''
+"""
 
 import reda
 import numpy as np
 
 # A config with the used channels/configurations has to be put in. This can be
-# a numpy array or a config.dat-file. It is important that normal and 
+# a numpy array or a config.dat-file. It is important that normal and
 # reciprocal measurements are taken for the check.
 
 configs = np.array([
@@ -23,9 +27,9 @@ configs = np.array([
 
 # The function can be executed with the following command. The first parameter
 # has to lead to the datapath of the measurement, the second to the config.dat
-# or config array and the third one denotes the output name. Optional 
-# parameters are the frequency range to compare the measurements to 
-# (input has to be in log10-space) and the error-percentage of the 
+# or config array and the third one denotes the output name. Optional
+# parameters are the frequency range to compare the measurements to
+# (input has to be in log10-space) and the error-percentage of the
 # measurements.
 
 reda.utils.eit_fzj_utils.testboard_evaluation(
