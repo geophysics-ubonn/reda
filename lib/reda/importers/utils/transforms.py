0c6e5a37e2ad04cb8f60c7873231e362c46d385b
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-12-30 22:19:03 +0100

    [transforms] Check that logical electrode position shifts are only applied of no electrode positions are available.

--- lib/reda/importers/utils/transforms.py
+++ lib/reda/importers/utils/transforms.py
@@ -7,6 +7,10 @@ design your own.
 class transform_electrodes_roll_along(object):
     """This function shifts all electrode numbers by a fixed offset, as
     commonly encountered for roll-a-long measurement schemes.
+
+    This transformation should only be used for logical abmn transformations,
+    i.e., in those cases were electrode positions are not available. Otherwise
+    roll-along should be realized using coordinate transformations.
     """
     def __init__(self, shiftby=0):
         """
@@ -18,5 +22,12 @@ class transform_electrodes_roll_along(object):
         self.shiftby = shiftby
 
     def transform(self, data, electrodes, topography):
+        if electrodes is not None:
+            raise Exception(
+                'Do not use the transform_electrodes_roll_along transform ' +
+                'object if you have electrode positions!. Perform a ' +
+                'roll-along alignment using the shiftby_xyz parameter of ' +
+                'the importer functions.'
+            )
         data[['a', 'b', 'm', 'n']] += self.shiftby
         return data, electrodes, topography
