9d5a79ec5bcecc44ee44cd6a5bcf6bcaf937c10a
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2021-12-13 20:59:44 +0100

    work on tsert file format

--- examples/09_file_formats/load_data.py
+++ examples/09_file_formats/load_data.py
@@ -1,23 +1,73 @@
 #!/usr/bin/env python
+"""
+https://docs.h5py.org/en/stable/quick.html#quick
+"""
 import os
 
 import h5py
+
 
 class hdf_importer(object):
     def __init__(self, filename):
         assert os.path.isfile(filename)
         self.filename = filename
+        self._check_hdf_file()
         f = h5py.File(filename, 'r')
         self.available_timesteps = list(f['ERT_DATA'].keys())
         f.close()
 
+    def _open_file(self):
+        return h5py.File(self.filename, 'r')
+
+    def _check_hdf_file(self):
+        """Apply various checks to the registered file
+
+        """
+        f = h5py.File(self.filename, 'r')
+        assert 'ERT_DATA' in f.keys()
+
     def load_all_timesteps(self, version):
         for timestep in self.available_timesteps:
             key = 'ERT_DATA/{}/version'.format(timestep)
+            print(key)
 
+    def summary(self, filename=None):
+        """Short summary of the filename
+        """
+        if filename is not None:
+            file_to_load = filename
+        else:
+            file_to_load = self.filename
 
+        f = h5py.File(file_to_load, 'r')
+        nr_timesteps = len(f['ERT_DATA'].keys())
+        print('Number of time steps: {}'.format(nr_timesteps))
+
+        # go through all timesteps and enumerate the versions
+        versions = {}
+        for timestep in f['ERT_DATA'].keys():
+            for version in f['ERT_DATA'][timestep]:
+                versions[version] = versions.get(version, 0) + 1
+        print('Available versions:')
+        for version, numbers in versions.items():
+            print(
+                ' - Version {} is present {} times'.format(version, numbers)
+            )
+
+    def add_metadata(self):
+        f = h5py.File('data.h5', 'a')
+        metadata = {
+            'a': 'pla',
+            'b': 'bum',
+        }
+        f.create_group('METADATA')
+
+        for key, item in metadata.items():
+            f['METADATA'].attrs[key] = item
+
+        f['METADATA'].attrs.keys()
 
 
 if __name__ == '__main__':
     obj = hdf_importer('data.h5')
-    print(obj.available_timesteps)
+    obj.summary()
--- examples/09_file_formats/plot_01_tsert.py
+++ examples/09_file_formats/plot_01_tsert.py
@@ -3,27 +3,38 @@
 """
 Using the TSERT file format
 ===========================
+
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_hdf.html
 """
 import reda
+# import h5py
+# import pandas as pd
 
 ert = reda.ERT()
 
-ert.import_crtomo_data('data/2018-04-13_11-00-25+00-00.dat', timestep=1)
-ert.import_crtomo_data('data/2018-06-01_09-00-43+00-00.dat', timestep=2)
-ert.import_crtomo_data('data/2018-08-02_09-00-14+00-00.dat', timestep=3)
+# version 1
+# ert.import_crtomo_data('data/2018-04-13_11-00-25+00-00.dat', timestep=1)
+# ert.import_crtomo_data('data/2018-06-01_09-00-43+00-00.dat', timestep=2)
+# ert.import_crtomo_data('data/2018-08-02_09-00-14+00-00.dat', timestep=3)
 
-import h5py
-import pandas as pd
+# version 2: MANY timesteps
+for i in range(0, 100):
+    ert.import_crtomo_data('data/2018-08-02_09-00-14+00-00.dat', timestep=i)
+
 
 g = ert.data.groupby('timestep')
 
 filename = 'data.h5'
 
 for timestep, item in g:
-    print(name)
+    # print(name)
     key = '/'.join((
         'ERT_DATA',
         '{}'.format(timestep),
         'base',
     ))
-    item.to_hdf(filename, key, append=True,)
+    item.to_hdf(
+        filename, key, append=True,
+        complevel=9,
+        complib='lzo',
+    )
