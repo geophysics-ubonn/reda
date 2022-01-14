6272b25063e06d592b4bdfa58a4e85b032f1aba8
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2022-01-14 21:11:45 +0100

    remove very old files that are not important anymore

--- BERT/create_datacontainer.py
+++
@@ -1,4 +0,0 @@
-data = pb.DataContainerERT()
-data.resize(3) # 3 Messungen
-data.set("a", [1, 2, 3]) # np arrays sollten auch gehen. Dasselbe für b, m, n, u, i, r, k, rhoa, etc.
-http://resistivity.net/index.php?id=unidata&type=1
