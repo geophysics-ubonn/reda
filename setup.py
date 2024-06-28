4cc71c4d2cf79b821380b275eb0542b42b3b23e4
Maximilian Weigand <mweigand@mweigand.net>
2024-06-28 16:22:49 +0200

    version 0.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 # python.exe setup.py bdist --format msi
 # to create a windows installer
 
-version_long = '0.2.1'
+version_long = '0.2.2'
 
 # package data
 os.chdir('lib/reda/testing')
