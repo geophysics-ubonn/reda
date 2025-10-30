dabc6dde3bc8d2a132a79b8aeca62ec69dfc3c4b
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2025-10-30 14:00:47 +0100

    v0.2.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 # python.exe setup.py bdist --format msi
 # to create a windows installer
 
-version_long = '0.2.5'
+version_long = '0.2.6'
 
 # package data
 os.chdir('lib/reda/testing')
